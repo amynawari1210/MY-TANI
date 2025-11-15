6d820337d18f413de1d350a37f6efbef5b02bc97
amynawari1210 <amynawari1210@gmail.com>
2025-11-15 10:20:08 +0800

    Refactor service worker install and fetch events

--- docs/sw.js
+++ docs/sw.js
@@ -1,10 +1,21 @@
-self.addEventListener('install', function(e){
-  e.waitUntil(caches.open('mytani-cache-v1').then(function(cache){
-    return cache.addAll(['index.html','manifest.json']);
-  }));
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open("mytani-cache-v1").then((cache) => {
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./manifest.json",
+        "./icon-192.png",
+        "./icon-512.png"
+      ]);
+    })
+  );
 });
-self.addEventListener('fetch', function(e){
-  e.respondWith(caches.match(e.request).then(function(response){
-    return response || fetch(e.request);
-  }));
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });
